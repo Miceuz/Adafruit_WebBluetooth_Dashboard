f4c424136c03b4d930e04107fd1e852680213bcf
Albertas Mickėnas <albertas@technarium.lt>
2023-09-29 09:57:50 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@
 import * as THREE from 'https://unpkg.com/three@0.119.1/build/three.module.js';
 
 // import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
-import {GLTFLoader} from 'https://threejs.org/examples/jsm/loaders/GLTFLoader.js';
+import {GLTFLoader} from 'https://unpkg.com/three@0.119.1/examples/jsm/loaders/GLTFLoader.js';
 
 let device;
 
